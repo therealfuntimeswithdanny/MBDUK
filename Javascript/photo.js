aabbd89e378ad41317c7edb40f7e4f5f556d42a9
Funtimeswithdanny <dannymorrisey1@outlook.com>
2024-08-27 17:39:00 -0400

    Updated photo.html

---
+++ Javascript/photo.js
@@ -0,0 +1,30 @@
+// Get the elements with class="column"
+var elements = document.getElementsByClassName("column");
+
+// Declare a loop variable
+var i;
+
+// Full-width images
+function one() {
+    for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "50%";  // IE10
+    elements[i].style.flex = "50%";
+  }
+}
+
+// Two images side by side
+function two() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "33%";  // IE10
+    elements[i].style.flex = "33%";
+  }
+}
+
+// Four images side by side
+function four() {
+  for (i = 0; i < elements.length; i++) {
+    elements[i].style.msFlex = "25%";  // IE10
+    elements[i].style.flex = "25%";
+  }
+}
+
